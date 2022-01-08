6f379e4254fa806cea9b6a9058a36f1e6615a3ab
Bang241002 <91303671+Bang241002@users.noreply.github.com>
2022-01-08 11:30:07 +0700

    Add files via upload

--- vidu_chuong_10.sql
+++ vidu_chuong_10.sql
@@ -13,14 +13,14 @@ Go
 --vidu3
 CREATE VIEW vwPersonDetails AS
 SELECT 
-	p.Title,
-	p.[FirstName],
-	p.[MiddleName],
-	p.[LastName],
-	e.[JobTitle]
-FROM [HumanResources].[Employee]e
+	p.Title
+	,p.[FirstName]
+	,p.[MiddleName]
+	,p.[LastName]
+	,e.[JobTitle]
+FROM [HumanResources].[Employee] e
 		INNER JOIN [Person].[Person] p
-		ON p.[BusinessEntityID] =e. [BusinessEntityID]
+		ON p.[BusinessEntityID] =e.[BusinessEntityID]
 
 --vidu4
 SELECT * FROM vwPersonDetails
@@ -145,5 +145,163 @@ Go
 
 
 --viidu19
+ALTER VIEW vwProduct Info AS
+SELECT ProductID, ProductNumber, Name, SafetyStookLeel, ReOrderProint
+FROM Production.Product;
+GO
+
+--vidu20
+DROP VIEW vwProduct Info
+Go
+
+--vidu21
+EXEC sp_helptext vwEmployee_Personal_datails
+GO
+
+--vidu22
+CREATE VIEW vwProduct_Details AS
+SELECT 
+ProducName,
+AVG(Rate) AS VerageRate FROM 
+Product_Details GROUP BY ProductName
+GO
+
+--vidu23
+CREATE VIEW vwProduct Info AS
+SELECT ProductID, ProductNumber, Name, SafetyStockLevel,
+ReOrderPoint 
+FROM Production.Product
+WHere SafetyStockLevel <=1000
+WITH CHECK OPTION;
+GO
+
+--vidu24
+UPDATE vwProductInfo SET SafetystockLevel = 2500
+WHERE ProductID = 321
+GO
+
+--vidu25
+CREATE VIEW vwNEWProductInfo WITH SCHEMABINDING AS
+SELECT ProductID, ProductNumber, SafetystockLevel 
+FROM Production.Product;
+GO
+
+--vidu26
+CREATE TABLE Customers
+(
+CustID int,
+CustName varchar(50),
+Address varchar(60)
+)
+GO
 
 
+--vidu27
+CREATE VIEW Custmoers AS
+SELECT * FROM Customers
+Go
+
+--vidu28
+SELECT * FROM vwCustomers
+Go
+
+--vidu29
+ALTER TABLE Customers ADD Age int 
+Go
+
+--vidu30
+
+SELECT * FROM vwCustomers
+GO
+
+--vidu31
+EXEC sp_Refreshview 'vwCustomers'
+GO
+
+--vidu32
+ALTER TABLE Production.Product ALTER COLUMN ProductID varchar(7)
+Go
+
+--vidu33
+EXECUTE xp_fileexist 'c:MyTest.txt'
+Go
+
+--vidu34
+CREATE PROCEDURE uspGetCustTerritory
+AS
+SELECT TOP 10 CustmoersID, Custmoers.TerritoryID, Sales.SalesTerritory.Name
+FROM Sales.Customer JOIN sales.salesTerritory On Sales.Customer.TerritoryID= Sales.SalesTerritory.TerritoryID
+Go
+
+--vidu35
+EXEC uspGetCustTerriTory
+
+GO
+
+--vidu36
+
+CREATE PROCEDURE uspGtSales @territoryvarchar(40) AS 
+SELECT BusinessEntitylD, B.SalesYTD, B.SalesLastYear FROM Sales.Salesperson A
+JOIN Sales.SalesTerritory B
+ON A.TerritoryID = B.TerritoryID WHERE
+B.Name = @territory;
+--Execute the storedprocedure EXEC 
+uspGetSales'Northwest'
+Go
+
+--vidu37
+CREATE PROCEDURE uspGetTotalSales @territory 
+varchar (40}, @sum int OUTPUT AS
+SELECT @sum=SUM(B.SalesYTD) FROM
+Sales.Salesperson A JOIN
+Sales.SalesTerritory B
+ONA.TerritoryID = B.TerritoryID
+WHERE B.Name = @territory 
+Go
+
+--vidu38
+DECLARE @sumsales money
+EXEc uspGetTotalSales'Northwest', @surnsales OUTPUT
+PRINT 'The year-to-date sales figure for this territory is ' +convert (varchar (100),
+@sumsales)
+GO
+
+--vidu39
+ALTER PROCEDURE [dbo].[uspGetTotals]
+@territory varchar = 40
+AS
+SELECT BusinessEntityID, B.SalesYTD, B.CostYTD, B.SalesLastYear FROM
+Sales.Salesperson A JOIN Sales.SalesTerritory B
+ON A. TerritoryID = B.TerritoryID
+WHERE B. Name = @territory;
+Go
+
+--vidu40
+DROP PROCEDURE uspGettotals 
+Go
+
+--vidu41
+CREATE PROCEDURE NestedProcedure AS BEGIN
+EXEC uspGetCustTerritory
+EXEC uspGetSales 'France'
+END
+GO
+
+--vidu42
+SELECT name, oBject_id, type, type_desc
+FROM sys.table;
+Go
+
+--vidu43
+SELECT TABLE_CÂTLOG, TABLE_SCHEMA, TABLE_NAME, TABLE_TYPE 
+FROM INFORMATION_SCHEMA.TAVLES;
+Go
+
+--vidu44
+SELECT SERVERPROPERTY('EDITION') aS EditionName;
+GO
+
+--vidu45
+SELECT session_id, login_time, program_name FROM sys.dm_exec_sessions
+WHERE login_name='sa' and is_user_process=1
+Go
